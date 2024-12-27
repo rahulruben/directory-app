a1cbf5287b9710aaed02c8295174cd91a93143ce
Rahulruben Mule <mulerahulruben@gmail.com>
2024-12-27 16:51:10 +0530

    Refatored conditional statement

--- src/app/components/tree-container/tree-container.component.ts
+++ src/app/components/tree-container/tree-container.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { TreeNodeComponent } from '../tree-node/tree-node.component';
 import { TreeNode } from '../../interfaces/tree-node.interface';
-import { NgFor } from '@angular/common';
 import { MOCK_FOLDER_STRUCTURE } from '../../mocks/mocks';
 
 @Component({
   selector: 'app-tree-container',
-  imports: [TreeNodeComponent, NgFor],
+  imports: [TreeNodeComponent],
   templateUrl: './tree-container.component.html',
   styleUrl: './tree-container.component.scss',
 })
